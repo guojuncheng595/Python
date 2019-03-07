f04a3374e76ecc97d1d0c3ce03826d567f52f7c1
root <402128964@qq.com>
2019-03-07 13:57:45 +0800

    爬取伯乐在线文章，存入数据库

--- linuxShare/ASpider/ASpider/utils/wukongwenda_requests.py
+++ linuxShare/ASpider/ASpider/utils/wukongwenda_requests.py
@@ -1,38 +1,29 @@
 # -*- coding: utf-8 -*-
 import requests
-import bs4
-from urllib.parse import urlencode
 try:
     import cookielib
 except:
     import http.cookiejar as cookielib
 
-# import re
+import re
 import json
+import bs4
+from requests.cookies import RequestsCookieJar
 
-# session = requests.session()
-#
-# agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
-# header = {
-#     "HOST":"www.yunpanjingling.com",
-#     "User-Agent": agent,
-#     "X-Requested-With": "XMLHttpRequest",
-#     "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-#     "Content-Length": "48",
-#     "Connection": "keep-alive",
-#     "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-#     "Accept-Encoding": "gzip, deflate, br",
-#     "Accept": "application/json, text/javascript, */*; q=0.01"
-# }
+session = requests.session()
+session.cookies = cookielib.LWPCookieJar(filename='cookies')
+session.cookies.save(ignore_discard=True)
+try:
+    session.cookies.load(ignore_discard=True)
+except:
+    print("Cookie 未能加载")
+
 #
 #
-# def get_cookies():
-#     response = requests.get("https://www.yunpanjingling.com",headers=header)
-#     if response.cookies:
-#         for key, value in response.cookies.items():
-#             return value
-#     else:
-#         return "none"
+
+#
+#
+
 #
 #
 # def get_csrf():
@@ -79,33 +70,79 @@ import json
 # wukongwenda_login("2670617835@qq.com", "g2670617835")
 # get_csrf()
 
-def ajaxRequest():
+header = {
+    "HOST":"www.yunpanjingling.com",
+    "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "Accept": "application/json, text/javascript, */*; q=0.01"
+}
 
+def get_xsrf_token():
+    response = requests.get("https://www.yunpanjingling.com/user/login", headers=header)
+    token = ""
+    if response.cookies:
+        for key, value in response.cookies.items():
+            if key == 'XSRF-TOKEN':
+                token += ("XSRF-TOKEN=" + value + ";")
+            elif key == '_session':
+                token += ("_session=" + value + ";")
+        print(token[:-1])
+        return token[:-1]
+    else:
+        return "none"
+
+def get_csrf_token():
+    # 获取csrf code   https://www.jianshu.com/p/887af1ab4200
+    response = requests.get("https://www.yunpanjingling.com/user/login", headers=header)
+
+    token = ""
+    ctoken = ""
+    if response.cookies:
+        cookie_jar = RequestsCookieJar()
+        cookie_jar.set("BAIDUID", "B1CCDD4B4BC886BF99364C72C8AE1C01:FG=1", domain="baidu.com")
+        for key, value in response.cookies.items():
+            if key == 'XSRF-TOKEN':
+                token += ("XSRF-TOKEN=" + value + ";")
+            elif key == '_session':
+                token += ("_session=" + value + ";")
+        print(token[:-1])
+        token = token[:-1]
+    else:
+        token = 'none'
+
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
+    soup.title.string[3:7]
+    ctoken = "none"
+    for meta in soup.select('meta'):
+        if meta.get('name') == 'csrf-token':
+            print(meta.get('content'))
+            ctoken = meta.get('content')
+    return [ctoken, token]
+
+
+def ajaxRequest():
     headers = {
-        "X-CSRF-TOKEN": "GDyx6XJQ6OajAmLC1D7PA9WbMXRswo1WqcS5u7Z2",
+        "X-CSRF-TOKEN": get_csrf_token()[0],
         "Accept": "application/json, text/javascript, */*; q=0.01",
-        "Cookie": "_ga=GA1.2.2102293619.1551623139; _gid=GA1.2.148598326.1551623139; XSRF-TOKEN=eyJpdiI6ImQzKzZocHJcL1pEMU9UWmZLaDE0YjhnPT0iLCJ2YWx1ZSI6Ind4WG5VWHVicEZyM2orcDlOYWlEWWplU2pqNENCTTgrRHhPenkrTkt4Rjdtc282UWlReEVqUHQzNmIydzdkRWpybWhUbjVpZzdkeGZzcWpjbnRZeExBPT0iLCJtYWMiOiIxYzc2NWUyOTY3OGQwNDllZTRiYTM0YjdhZjNmZGVlYjBkYmRkY2Y1ZTlkYTJlYjI5NDBkZTA5YjU0ODIyMmU3In0%3D; _session=eyJpdiI6InZWTTFJc0RvUTdJb2NuSUZOeTBhVmc9PSIsInZhbHVlIjoibGl1TnYwUW5wU1dDNW5halZjRmJkV0M0dW9MNktMbmNPUjg4akRLaHhXcEMzWDVjWlZtUzdDY1E4Uks2Z2haRVJtMSt3TGdTYjBadW5ZS2pOdTBuUlE9PSIsIm1hYyI6ImFjN2VkOTJjMzQ1MGM3NWE0OWY3MjE5OGRkNTk5MThkMjEzNWUyNjE2ZTk2OGY0MDVkMWMxYzE0NzlhY2U1NDIifQ%3D%3D; _gat_gtag_UA_109184535_5=1",
-        "Host": "https://www.yunpanjingling.com",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0",
+        "Cookie": get_csrf_token()[1],
         "Content-Type": "application/json; charset=UTF-8"
     }
-    # email=14124123512%40qq.com&password=sfasdd&remember=true
     datas = {'email': '2670617835@qq.com', 'password': 'g2670617835', 'remember': 'true'}
-    # datas = {}
-    # datas["email"] = "2670617835@qq.com"
-    # datas["password"] = "g2670617835"
-    # datas["remember"] = "true"
-
     url = "https://www.yunpanjingling.com/user/login"
-    # session = requests.session()
+    response = session.post(url=url, data=json.dumps(datas), headers=headers)
+    if response.status_code == 200:
+        print("返回码：" + str(response.status_code))
+    else:
+        print("返回码：" + str(response.status_code))
+    session.cookies.save()
+    cookie = session.cookies
+    print(cookie)
 
-
-    print(type(datas))
-    print(type(json.dumps(datas)))
-    requ = requests.post(url=url, data=json.dumps(datas), headers = headers)
-    requ.encoding = "UTF-8"
-
-    res = requ.text
-    print(res)
 
 ajaxRequest()
+# test = get_csrf_token()
+# print(test[0])
+# print(test[1])
+# get_xsrf_token()
+
+# get_csrf_token()
