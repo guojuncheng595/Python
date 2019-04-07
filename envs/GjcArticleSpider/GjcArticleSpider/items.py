9f4e11cfe6d74c590574b2560873ba3115e4fba9
guojuncheng595 <402128964@qq.com>
2019-04-07 10:58:47 +0800

    伯乐在线爬去并且数据入库

--- envs/GjcArticleSpider/GjcArticleSpider/items.py
+++ envs/GjcArticleSpider/GjcArticleSpider/items.py
@@ -5,7 +5,11 @@
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
 
+import re
 import scrapy
+import datetime
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import MapCompose, TakeFirst, Join
 
 
 class GjcarticlespiderItem(scrapy.Item):
@@ -13,16 +17,68 @@ class GjcarticlespiderItem(scrapy.Item):
     # name = scrapy.Field()
     pass
 
+
+def add_jobbole(value):
+    return value+"-jobbole1"
+
+
+def date_convert(value):
+    try:
+        create_date = datetime.datetime.strftime(value, "%Y/%m/%d").date()
+    except Exception as e:
+        create_date = datetime.datetime.now().date()
+    return create_date
+
+
+class ArticleItemLoader(ItemLoader):
+    # 自定义Itemloader
+    default_output_processor = TakeFirst()
+
+
+def return_value(value):
+    return value
+
+
+def get_num(value):
+    match_re = re.match(".*(\d+).*", value)
+    if match_re:
+        nums = int(match_re.group(1))
+    else:
+        nums = 0
+    return nums
+
+
+def remove_comment_tag(value):
+    # 去掉tag中提取的评论
+    if '评论' in value:
+        return ""
+    else:
+        return value
+
+
 # 自定义Item
 class JobBoleArticleItem(scrapy.Item):
     title = scrapy.Field()
-    create_date = scrapy.Field()
+    create_date = scrapy.Field(
+        input_processor=MapCompose(date_convert),
+    )
     url = scrapy.Field()
     url_object_id = scrapy.Field()
-    front_image_url = scrapy.Field()
+    front_image_url = scrapy.Field(
+        output_processor=MapCompose(return_value)
+    )
     front_image_path = scrapy.Field()
-    praise_nums = scrapy.Field()
-    comment_nums = scrapy.Field()
-    fav_nums = scrapy.Field()
-    tags = scrapy.Field()
+    praise_nums = scrapy.Field(
+        input_processor=MapCompose(get_num)
+    )
+    comment_nums = scrapy.Field(
+        input_processor=MapCompose(get_num)
+    )
+    fav_nums = scrapy.Field(
+        input_processor=MapCompose(get_num)
+    )
+    tags = scrapy.Field(
+        input_processor=MapCompose(remove_comment_tag),
+        output_processor=Join(",")
+    )
     content = scrapy.Field()
--- envs/GjcArticleSpider/GjcArticleSpider/pipelines.py
+++ envs/GjcArticleSpider/GjcArticleSpider/pipelines.py
@@ -21,10 +21,12 @@ class GjcarticlespiderPipeline(object):
 # 图片处理Pipeline
 class ArticleImagePipeline(ImagesPipeline):
     def item_completed(self, results, item, info):
-        for ok, value in results:
-            image_file_path = value["path"]
-        item["front_image_path"] = image_file_path
+        if "front_image_url" in item:
+            for ok, value in results:
+                image_file_path = value["path"]
+            item["front_image_path"] = image_file_path
         return item
+
 
 # 链接数据库操作 -- 拆用同步的机制写入MySQL数据库
 class MysqlPipeline(object):
